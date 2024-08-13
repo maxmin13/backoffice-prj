c83aeff772f63db012ed41674948daa9c81768f6
maxmin <vagrant@maxmin.com>
2024-08-13 16:51:43 +0100

    dao project unit tests

--- project/persistence/backoffice-jdbc-dao/src/main/java/it/maxmin/plain/dao/impl/operation/address/AddressQueryConstants.java
+++ project/persistence/backoffice-jdbc-dao/src/main/java/it/maxmin/plain/dao/impl/operation/address/AddressQueryConstants.java
@@ -3,7 +3,16 @@ package it.maxmin.plain.dao.impl.operation.address;
 public enum AddressQueryConstants {
 	;
 
-	public static final String SELECT_ADDRESSES_BY_USER_ID = "SELECT a.AddressId, a.Address, a.City, a.StateId, a.Region, a.PostalCode FROM Address a INNER JOIN UserAddress u ON a.AddressId = u.AddressId WHERE u.UserId = :userId ORDER BY a.addressId";
-	public static final String UPDATE_ADDRESS = "UPDATE Address SET Address=:address, City=:city, StateId=:stateId, Region=:region, PostalCode=:postalCode WHERE AddressId=:addressId";
+	public static final String SELECT_ADDRESSES_BY_USER_ID = ""
+			+ "SELECT a.AddressId, a.Address, a.City, a.StateId, a.Region, a.PostalCode "
+			+ "FROM Address a "
+			+ "INNER JOIN UserAddress u ON a.AddressId = u.AddressId "
+			+ "WHERE u.UserId = :userId "
+			+ "ORDER BY a.addressId";
+	
+	public static final String UPDATE_ADDRESS = ""
+			+ "UPDATE Address "
+			+ "SET Address=:address, City=:city, StateId=:stateId, Region=:region, PostalCode=:postalCode "
+			+ "WHERE AddressId=:addressId";
 
 }
