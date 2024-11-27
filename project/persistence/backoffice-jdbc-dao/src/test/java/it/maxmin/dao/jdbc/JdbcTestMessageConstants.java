904268bceac6a57094950478d6700d2a724db6e4
maxmin <vagrant@maxmin.com>
2024-11-27 10:20:19 +0000

    dao project unit tests

--- project/persistence/backoffice-jdbc-dao/src/test/java/it/maxmin/dao/jdbc/JdbcTestMessageConstants.java
+++ project/persistence/backoffice-jdbc-dao/src/test/java/it/maxmin/dao/jdbc/JdbcTestMessageConstants.java
@@ -3,10 +3,10 @@ package it.maxmin.dao.jdbc;
 public enum JdbcTestMessageConstants {
 	;
 	
-	public static final String ERROR_DEPARTMENT_NOT_FOUND_MSG = "Error department not found";
-	public static final String ERROR_STATE_NOT_FOUND_MSG = "Error state not found";
-	public static final String ERROR_ROLE_NOT_FOUND_MSG = "Error role not found";
 	public static final String ERROR_ADDRESS_NOT_FOUND_MSG = "Error address not found";
+	public static final String ERROR_DEPARTMENT_NOT_FOUND_MSG = "Error department not found";
+	public static final String ERROR_ROLE_NOT_FOUND_MSG = "Error role not found";
+	public static final String ERROR_STATE_NOT_FOUND_MSG = "Error state not found";
 	public static final String ERROR_USER_NOT_FOUND_MSG = "Error user not found";
 
 }
