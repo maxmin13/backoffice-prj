25e7b1249a6137151772311324b2abcaadf158cb
vagrant <vagrant@maxmin.com>
2025-01-07 10:02:06 +0000

    Jpa integration tests

---
+++ project/persistence/backoffice-jpa-dao/src/test/java/it/maxmin/dao/jpa/step/BaseStep.java
@@ -0,0 +1,14 @@
+package it.maxmin.dao.jpa.step;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import it.maxmin.dao.jpa.config.JpaDaoSpringContextCfg;
+
+@CucumberContextConfiguration
+@ContextConfiguration(classes = {JpaDaoSpringContextCfg.class})
+@TestPropertySource("classpath:jndi/jndi-integration.properties") 
+public class BaseStep {
+
+}
