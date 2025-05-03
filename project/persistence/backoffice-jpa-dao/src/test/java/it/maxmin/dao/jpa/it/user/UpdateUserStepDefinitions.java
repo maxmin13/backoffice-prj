6435fe35a5b5cab6e65c5955a5c2d2899416f928
vagrant <vagrant@maxmin.com>
2025-05-03 15:12:16 +0100

    JPA integration tests

--- project/persistence/backoffice-jpa-dao/src/test/java/it/maxmin/dao/jpa/it/user/UpdateUserStepDefinitions.java
+++ project/persistence/backoffice-jpa-dao/src/test/java/it/maxmin/dao/jpa/it/user/UpdateUserStepDefinitions.java
@@ -2,6 +2,7 @@ package it.maxmin.dao.jpa.it.user;
 
 import static it.maxmin.common.constant.MessageConstants.ERROR_OBJECT_NOT_FOUND_MSG;
 import static it.maxmin.dao.jpa.it.constant.StepConstants.USER;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,7 +17,7 @@ import it.maxmin.dao.jpa.it.context.ScenarioItemContext;
 import it.maxmin.dao.jpa.it.error.StepErrorManager;
 import it.maxmin.model.jpa.dao.entity.User;
 
-public class CreateUserStepDefinitions {
+public class UpdateUserStepDefinitions {
 
 	private LogScenarioUtil logScenarioUtil;
 	private ScenarioItemContext scenarioItemContext;
@@ -26,7 +27,7 @@ public class CreateUserStepDefinitions {
 	private UserDao userDao;
 
 	@Autowired
-	public CreateUserStepDefinitions(ScenarioItemContext scenarioItemContext, StepErrorManager stepErrorManager,
+	public UpdateUserStepDefinitions(ScenarioItemContext scenarioItemContext, StepErrorManager stepErrorManager,
 			FeatureUserHelper featureUserHelper, MessageService messageService, LogScenarioUtil logScenarioUtil,
 			UserDao userDao) {
 		this.logScenarioUtil = logScenarioUtil;
@@ -37,26 +38,36 @@ public class CreateUserStepDefinitions {
 		this.userDao = userDao;
 	}
 
-	@Given("I want to create the following user")
-	public void i_want_to_create_the_user(DataTable user) {
-		User data = featureUserHelper.buildUser(user);
-		logScenarioUtil.log("I want to create a user");
-		logScenarioUtil.log("{0}", user);
-		scenarioItemContext.setItem(USER, data);
-	}
-
-	@When("I create the user")
-	public void create_user() {
+	@Given("I want to update the user")
+	public void i_want_to_update_the_user() {
 		User user = (User) scenarioItemContext.getItem(USER).orElseThrow(
 				() -> new JpaDaoTestException(messageService.getMessage(ERROR_OBJECT_NOT_FOUND_MSG, "user")));
+		logScenarioUtil.log("I want to update the user");
+		logScenarioUtil.log("{0}", user);
+	}
+
+	@When("I update the user")
+	public void update_user(DataTable user) {
+		assertNotNull(user);
+		User data = featureUserHelper.buildUser(user);
+
+		User u = (User) scenarioItemContext.getItem(USER).orElseThrow(
+				() -> new JpaDaoTestException(messageService.getMessage(ERROR_OBJECT_NOT_FOUND_MSG, "user")));
+
+		u.setAccountName(data.getAccountName());
+		u.setFirstName(data.getFirstName());
+		u.setLastName(data.getLastName());
+		u.setBirthDate(data.getBirthDate());
+
 		try {
-			userDao.create(user);
-			logScenarioUtil.log("inserted new user {0} in the database", user);
+			User updated = userDao.update(u);
+			// replace the managed entity
+			scenarioItemContext.setItem(USER, updated);
+			logScenarioUtil.log("updated user {0} in the database", updated);
 		}
 		catch (Exception e) {
 			logScenarioUtil.log("{0}", e);
-			stepErrorManager.addError("update error", e);
+			stepErrorManager.addError("insert error", e);
 		}
 	}
-
 }
